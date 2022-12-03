17b8bad686797648ccd615805a272807008938b3
Rodrigo Monclair <rmonclair@gmail.com>
2022-12-03 14:21:33 -0300

    primeira iteração do purchase ok

--- routes/purchase.router.js
+++ routes/purchase.router.js
@@ -1,18 +1,18 @@
-import express from 'express'
-import PurchaseModel from '../models/purchase.model.js'
-import AlbumModel from '../models/album.model.js'
+import express from "express";
+import PurchaseModel from "../models/purchase.model.js";
+import AlbumModel from "../models/album.model.js";
 
-const purchaseRoute = express.Router()
+const purchaseRoute = express.Router();
 
-purchaseRoute.post("/purchase", async (req, res)=>{
-    
-})
+purchaseRoute.post("/purchase/:albumId", async (req, res) => {
+  try {
+    const { albumId } = req.params;
+    const newPurchase = await PurchaseModel.create({...req.body, album: albumId,});
+    return res.status(201).json(newPurchase);
+  } catch (error) {
+    console.log(error);
+    return res.status(400).json(error.errors);
+  }
+});
 
-
-
-
-
-
-
-
-export default purchaseRoute
+export default purchaseRoute;
